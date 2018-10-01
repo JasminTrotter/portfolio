952b15caffc2dc565151232478d0958895d848b9
Jasmin Trotter <jasminmtrotter@gmail.com>
2018-10-01 16:42:26 -0700

    styles

--- index.js
+++ index.js
@@ -3,6 +3,26 @@
 
 $(document).ready(function() {
 
+
+  //expand and collapse project details
+  $(listenExpand);
+
+  function listenExpand() {
+    $('.tech').on('click', '.more-button', function (event) {
+      $(event.currentTarget).next('.tech-list').removeClass('hidden');
+      $(event.currentTarget).addClass('hidden');
+      listenCollapse();
+    });
+  }
+
+  function listenCollapse() {
+    $('.less-button').click((e) => {
+      $(e.currentTarget).parent().addClass('hidden');
+      $(e.currentTarget).closest('.tech-list').prev().removeClass('hidden');
+      listenExpand();
+    });
+  }
+
 	// scroll on navbar links
   $('#home-link').click(() => {
     $('html, body').animate({
@@ -12,13 +32,13 @@ $(document).ready(function() {
 
   $('#about-me-link').click(() => {
     $('html, body').animate({
-      scrollTop: ($('#about-me-anchor').offset().top - 100)
+      scrollTop: ($('#about-me-anchor').offset().top - 80)
     },1000);
   });
 
   $('#projects-link').click(() => {
     $('html, body').animate({
-      scrollTop: ($('#projects-anchor').offset().top - 100)
+      scrollTop: ($('#projects-anchor').offset().top - 80)
     },1000);
   });
 
